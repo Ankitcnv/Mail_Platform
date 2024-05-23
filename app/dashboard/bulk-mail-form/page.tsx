bfdf18c4dcabb51e820ef0bc6f0fcdd23f091a6f
Ankit <ankitchaudharycnv.gmail.com>
2024-05-23 17:54:37 +0530

    added suspense

--- app/dashboard/bulk-mail-form/page.tsx
+++ app/dashboard/bulk-mail-form/page.tsx
@@ -2,6 +2,7 @@
 import submitFormData from "@/actions/Submit-Form-Data";
 import { Button } from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -67,118 +68,120 @@ const Bulk = () => {
   // };
 
   return (
-    <div className="flex justify-center items-center mt-8">
-      <div className="mx-auto max-w-scree-nxl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-lg text-center">
-          <h1 className="text-2xl font-bold sm:text-3xl">{title}</h1>
-          <p className="mt-4 mb-3 text-gray-500">
-            Increase Your Productivity by Integrating with Bulk Email Feature.
-          </p>
-        </div>
-        <div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              // @ts-ignore
-              const formData = new FormData(e.target);
-              handleBroadcast(formData);
-            }}
-            className="mx-auto max-w-md space-y-4">
-            <div>
-              <div className="relative">
-                <input
-                  type="text"
-                  className="w-full mb-2 rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
-                  placeholder="Enter The subject of the mail."
-                  name="subject"
-                />
-              </div>
-
-              <div className="relative">
-                <input
-                  type="text"
-                  className="w-full rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
-                  placeholder="Enter The heading."
-                  name="heading"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="relative mb-2">
-                <div className="">
-                  <img
-                    className="w-56 h-56 p-2 object-cover border border-zinc-900 rounded-lg text-sm shadow-sm items-center justify-center"
-                    src={url as string}
-                    // @ts-ignore
-                    name="file"
-                  />
-                </div>
-                <input
-                  type="text"
-                  value={url as string}
-                  className="hidden"
-                  name="url"
-                />
-              </div>
-
-              <div className="relative">
-                <textarea
-                  className="w-full rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
-                  rows={3}
-                  cols={4}
-                  placeholder="Main Text Area."
-                  name="textarea"
-                />
-              </div>
-            </div>
-            <div>
-              {inputPairs.map((inputPair, index) => (
-                <div key={index} className="flex  gap-1 p-[2px] m-[3px]">
+    <Suspense>
+      <div className="flex justify-center items-center mt-8">
+        <div className="mx-auto max-w-scree-nxl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-lg text-center">
+            <h1 className="text-2xl font-bold sm:text-3xl">{title}</h1>
+            <p className="mt-4 mb-3 text-gray-500">
+              Increase Your Productivity by Integrating with Bulk Email Feature.
+            </p>
+          </div>
+          <div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                // @ts-ignore
+                const formData = new FormData(e.target);
+                handleBroadcast(formData);
+              }}
+              className="mx-auto max-w-md space-y-4">
+              <div>
+                <div className="relative">
                   <input
                     type="text"
-                    value={inputPair.key}
-                    className="outline outline-1 outline-zinc-700 p-1 rounded-md"
-                    placeholder="button name"
-                    onChange={(e) =>
-                      handleInputChange(index, "key", e.target.value)
-                    }
+                    className="w-full mb-2 rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
+                    placeholder="Enter The subject of the mail."
+                    name="subject"
                   />
+                </div>
+
+                <div className="relative">
                   <input
                     type="text"
-                    value={inputPair.value}
-                    className="outline outline-1 outline-zinc-700 p-1 rounded-md "
-                    placeholder="link."
-                    onChange={(e) =>
-                      handleInputChange(index, "value", e.target.value)
-                    }
+                    className="w-full rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
+                    placeholder="Enter The heading."
+                    name="heading"
                   />
-                  <button
-                    type="button"
-                    className="bg-red-400 outline outline-1 outline-zinc-900 p-1 m-1 rounded-lg text-red-800"
-                    onClick={() => removeInputPair(index)}>
-                    Remove
-                  </button>
                 </div>
-              ))}
-              <button
-                type="button"
-                onClick={addInputPair}
-                className=" text-black text-sm hover:text-red-700">
-                Add Button
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <Button
-                type="submit"
-                disabled={loading}
-                className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white">
-                {loading ? "Broadcast...." : "Broadcast"}
-              </Button>
-            </div>
-          </form>
+              </div>
+              <div>
+                <div className="relative mb-2">
+                  <div className="">
+                    <img
+                      className="w-56 h-56 p-2 object-cover border border-zinc-900 rounded-lg text-sm shadow-sm items-center justify-center"
+                      src={url as string}
+                      // @ts-ignore
+                      name="file"
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    value={url as string}
+                    className="hidden"
+                    name="url"
+                  />
+                </div>
+
+                <div className="relative">
+                  <textarea
+                    className="w-full rounded-lg p-1 text-sm shadow-sm border-zinc-700 outline outline-1"
+                    rows={3}
+                    cols={4}
+                    placeholder="Main Text Area."
+                    name="textarea"
+                  />
+                </div>
+              </div>
+              <div>
+                {inputPairs.map((inputPair, index) => (
+                  <div key={index} className="flex  gap-1 p-[2px] m-[3px]">
+                    <input
+                      type="text"
+                      value={inputPair.key}
+                      className="outline outline-1 outline-zinc-700 p-1 rounded-md"
+                      placeholder="button name"
+                      onChange={(e) =>
+                        handleInputChange(index, "key", e.target.value)
+                      }
+                    />
+                    <input
+                      type="text"
+                      value={inputPair.value}
+                      className="outline outline-1 outline-zinc-700 p-1 rounded-md "
+                      placeholder="link."
+                      onChange={(e) =>
+                        handleInputChange(index, "value", e.target.value)
+                      }
+                    />
+                    <button
+                      type="button"
+                      className="bg-red-400 outline outline-1 outline-zinc-900 p-1 m-1 rounded-lg text-red-800"
+                      onClick={() => removeInputPair(index)}>
+                      Remove
+                    </button>
+                  </div>
+                ))}
+                <button
+                  type="button"
+                  onClick={addInputPair}
+                  className=" text-black text-sm hover:text-red-700">
+                  Add Button
+                </button>
+              </div>
+              <div className="flex items-center justify-between">
+                <Button
+                  type="submit"
+                  disabled={loading}
+                  className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white">
+                  {loading ? "Broadcast...." : "Broadcast"}
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -4,6 +4,7 @@ import SubmitButton from "./Button";
 import Image from "next/image";
 import AddFormActions from "@/actions/Login-Action";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import toast from "react-hot-toast";
 
 const LoginPage = () => {
@@ -17,77 +18,84 @@ const LoginPage = () => {
     null;
   }
   return (
-    <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
-            alt=""
-            src="/home.svg"
-            className="absolute inset-0 object-cover w-[800px] h-full"
-          />
-        </aside>
+    <Suspense>
+      <section className="bg-white">
+        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+          <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+            <img
+              alt=""
+              src="/home.svg"
+              className="absolute inset-0 object-cover w-[800px] h-full"
+            />
+          </aside>
 
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <Image src={"/Logo.png"} alt="logo-image" width={130} height={50} />
-            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to CNV Money
-            </h1>
+          <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+            <div className="max-w-xl lg:max-w-3xl">
+              <Image
+                src={"/Logo.png"}
+                alt="logo-image"
+                width={130}
+                height={50}
+              />
+              <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Welcome to CNV Money
+              </h1>
 
-            <p className="mt-4 leading-relaxed text-gray-500 text-sm sm:text-xl">
-              An asset management company (AMC) is a firm that invests pooled
-              funds from clients, putting the capital to work through different
-              investments including stocks, bonds, real estate.
-            </p>
-            {error == "not_authenticated" && (
-              <p className="text-red-600">Not Authenticated.</p>
-            )}
-            <form
-              action={AddFormActions}
-              className="mt-8 grid grid-cols-6 gap-6">
-              <div className="col-span-6">
-                <label
-                  htmlFor="Email"
-                  className="block text-sm font-medium text-gray-700">
-                  {" "}
-                  Email{" "}
-                </label>
+              <p className="mt-4 leading-relaxed text-gray-500 text-sm sm:text-xl">
+                An asset management company (AMC) is a firm that invests pooled
+                funds from clients, putting the capital to work through
+                different investments including stocks, bonds, real estate.
+              </p>
+              {error == "not_authenticated" && (
+                <p className="text-red-600">Not Authenticated.</p>
+              )}
+              <form
+                action={AddFormActions}
+                className="mt-8 grid grid-cols-6 gap-6">
+                <div className="col-span-6">
+                  <label
+                    htmlFor="Email"
+                    className="block text-sm font-medium text-gray-700">
+                    {" "}
+                    Email{" "}
+                  </label>
 
-                <input
-                  type="email"
-                  id="Email"
-                  name="email"
-                  placeholder="Enter your Email."
-                  className="mt-1 p-2 w-full h-10 rounded-md border-gray-500 text-sm text-gray-700 shadow-sm"
-                />
-              </div>
+                  <input
+                    type="email"
+                    id="Email"
+                    name="email"
+                    placeholder="Enter your Email."
+                    className="mt-1 p-2 w-full h-10 rounded-md border-gray-500 text-sm text-gray-700 shadow-sm"
+                  />
+                </div>
 
-              <div className="col-span-6 ">
-                <label
-                  htmlFor="Password"
-                  className="block text-sm font-medium text-gray-700">
-                  {" "}
-                  Password{" "}
-                </label>
+                <div className="col-span-6 ">
+                  <label
+                    htmlFor="Password"
+                    className="block text-sm font-medium text-gray-700">
+                    {" "}
+                    Password{" "}
+                  </label>
 
-                <input
-                  type="password"
-                  id="Password"
-                  placeholder="Enter your Password."
-                  name="password"
-                  className={`mt-1 p-2 w-full h-10 rounded-md border-red-500 bg-white text-sm text-gray-700 shadow-sm 
+                  <input
+                    type="password"
+                    id="Password"
+                    placeholder="Enter your Password."
+                    name="password"
+                    className={`mt-1 p-2 w-full h-10 rounded-md border-red-500 bg-white text-sm text-gray-700 shadow-sm 
                   }`}
-                />
-              </div>
+                  />
+                </div>
 
-              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
-                <SubmitButton />
-              </div>
-            </form>
-          </div>
-        </main>
-      </div>
-    </section>
+                <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                  <SubmitButton />
+                </div>
+              </form>
+            </div>
+          </main>
+        </div>
+      </section>
+    </Suspense>
   );
 };
 
